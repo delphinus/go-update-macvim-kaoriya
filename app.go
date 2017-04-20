4478ae7e277174b3b5cfd6d4f3e20c177cf6bb8f
delphinus <delphinus@remora.cx>
2017-04-20 09:29:40 +0900

    Add replacer

--- app.go
+++ app.go
@@ -6,7 +6,9 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"net/http"
+	"regexp"
 
+	"github.com/Songmu/prompter"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 	"gopkg.in/cheggaaa/pb.v1"
@@ -63,8 +65,53 @@ func (g *Gumk) Run() error {
 		return errors.Wrap(err, "error in Wait")
 	}
 
-	fmt.Printf("dmgHash: %x\n", dmg.Sum(nil))
-	fmt.Printf("appcastHash: %x\n", appcast.Sum(nil))
-	fmt.Printf("release: %d\n", len(release.String()))
+	version, err := g.findVersion(release.Bytes())
+	if err != nil {
+		return errors.Wrap(err, "error in findVersion")
+	}
+
+	f := NewFormula(g.formula, g.tag, version, dmg.Sum(nil), appcast.Sum(nil))
+	e, err := f.read()
+	if err != nil {
+		return errors.Wrap(err, "error in read")
+	}
+
+	if !g.confirmProceed(f, e) {
+		return errors.New("cancelled")
+	}
+
+	if err := f.save(e); err != nil {
+		return errors.Wrap(err, "error in save")
+	}
+
+	fmt.Print("saved successfully")
+
 	return nil
+}
+
+var versionRe = regexp.MustCompile(`Vim (\d\.\d)`)
+
+func (g *Gumk) findVersion(s []byte) ([]byte, error) {
+	sm := versionRe.FindSubmatch(s)
+	if len(sm) < 2 || len(sm[1]) != 3 {
+		return nil, errors.New("cannot find version string")
+	}
+	return sm[1], nil
+}
+
+func (g *Gumk) confirmProceed(f Formula, e element) bool {
+	fmt.Printf(`found:
+  tag:     %s
+  version: %s
+  dmg:     %s
+  appcast: %s
+`, string(e.tag), string(e.version), string(e.dmg), string(e.appcast))
+	fmt.Print(`to update:
+  tag:     %s
+  version: %s
+  dmg:     %s
+  appcast: %s
+`, string(f.tag), string(f.version), string(f.dmg), string(f.appcast))
+
+	return prompter.YN("proceed?", false)
 }
---
+++ formula.go
@@ -0,0 +1,79 @@
+package gumk
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+
+	"github.com/pkg/errors"
+)
+
+type element struct {
+	tag, version, dmg, appcast []byte
+}
+
+// Formula is a struct for formulas
+type Formula struct {
+	path string
+	text []byte
+	element
+}
+
+// NewFormula returns a new Formula instance
+func NewFormula(path, tag string, version, dmg, appcast []byte) Formula {
+	return Formula{
+		path: path,
+		element: element{
+			tag:     []byte(tag),
+			version: version,
+			dmg:     dmg,
+			appcast: appcast,
+		},
+	}
+}
+
+var formulaRe = regexp.MustCompile(`else
+    version '(\d\.\d):(\d+)'
+    sha256 '([\da-f]+)'`)
+var appcastRe = regexp.MustCompile(`(?<=checkpoint: ')[\da-f]+`)
+
+func (f *Formula) read() (element, error) {
+	e := element{}
+	text, err := ioutil.ReadFile(f.path)
+	if err != nil {
+		return e, errors.Wrap(err, "error in ReadFile")
+	}
+
+	b := formulaRe.FindSubmatch(text)
+	if len(b) != 4 {
+		return e, errors.Wrap(err, "cannot find elements")
+	}
+
+	a := appcastRe.Find(text)
+	if len(a) == 0 {
+		return e, errors.Wrap(err, "cannot find appcast")
+	}
+
+	f.text = text
+	e.appcast = a
+	e.version = b[1]
+	e.tag = b[2]
+	e.dmg = b[3]
+	return e, nil
+}
+
+var formulaReplace = `else
+    version '%s:%s'
+    sha256 '%s'`
+var appcastReplace = `%s`
+
+func (f *Formula) save(e element) error {
+	formulaRepl := []byte(fmt.Sprintf(formulaReplace, e.version, e.tag, e.dmg))
+	f.text = formulaRe.ReplaceAll(f.text, formulaRepl)
+	appcastRepl := []byte(fmt.Sprintf(appcastReplace, e.appcast))
+	f.text = appcastRe.ReplaceAll(f.text, appcastRepl)
+	if err := ioutil.WriteFile(f.path, f.text, 0644); err != nil {
+		return errors.Wrap(err, "error in WriteFile")
+	}
+	return nil
+}
